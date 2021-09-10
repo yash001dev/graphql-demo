26b8b7a036fcfccb0997db1e0b9857bba323e169
yash_sanghavi_linux <y.sanghavi001@gmail.com>
2021-09-10 11:57:49 +0530

    add user projects

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_10_061619) do
+ActiveRecord::Schema.define(version: 2021_09_10_062443) do
+
+  create_table "projects", force: :cascade do |t|
+    t.string "title"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_projects_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -24,4 +32,5 @@ ActiveRecord::Schema.define(version: 2021_09_10_061619) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "projects", "users"
 end
